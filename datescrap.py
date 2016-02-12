db8e902fe9fca2ffc1cb613cef211c8213815151
Rafael D. Santos <santosdrafael@gmail.com>
2016-02-12 21:07:17 -0200

    Correção de varios bugs

--- datescrap.py
+++ datescrap.py
@@ -1,7 +1,17 @@
 import PyPDF2
 import re
+import urllib.request, urllib.parse, urllib.error
+import requests
+from datetime import date
 
-pdfFileObj = open('academico_2015_unificado.pdf', 'rb')
+dt = date.today()
+YEAR = str(dt.year)
+LAST_YEAR = str(int(dt.year)-1)
+url = 'http://www.prograd.ufscar.br/calendarios/academico_presenciais_'+YEAR+'.pdf'
+urllib.request.urlretrieve(url, 'academico_'+YEAR+'_unificado.pdf')
+
+pdfFileObj = open('academico_'+YEAR+'_unificado.pdf', 'rb')
+
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 
 pageObj = pdfReader.getPage(0)
@@ -42,12 +52,12 @@ string = re.sub(' [0-9]+ [0-9]+ [0-9]+ ','', string)
 string = re.sub(' [0-9]+ [0-9]+ ','', string)
 string = re.sub('    [0-9]+   ','', string)
 
-string = string.replace('2015/1','AAAA')
-string = string.replace('2015/2','BBBB')
-string = string.replace('2014/1','CCCC')
-string = string.replace('2014/2','DDDD')
-string = string.replace('2014','EEEE')
-string = string.replace('2015','FFFF')
+string = string.replace(YEAR+'/1','AAAA')
+string = string.replace(YEAR+'/2','BBBB')
+string = string.replace(LAST_YEAR+'/1','CCCC')
+string = string.replace(LAST_YEAR+'/2','DDDD')
+string = string.replace(LAST_YEAR,'EEEE')
+string = string.replace(YEAR,'FFFF')
 
 #coloca um \n sempre que encontrar uma data
 string = re.sub(r' ([0-9][0-9])',r'\n\1', string)
@@ -58,27 +68,29 @@ string = re.sub('e\n',r'e ', string)
 
 string = re.sub('  ',' ', string)
 
-string = string.replace('AAAA','2015/1')
-string = string.replace('BBBB','2015/2')
-string = string.replace('CCCC','2014/1')
-string = string.replace('DDDD','2014/2')
-string = string.replace('EEEE','2014')
-string = string.replace('FFFF','2015')
+string = string.replace('AAAA',YEAR+'/1')
+string = string.replace('BBBB',YEAR+'/2')
+string = string.replace('CCCC',LAST_YEAR+'/1')
+string = string.replace('DDDD',LAST_YEAR+'/2')
+string = string.replace('EEEE',LAST_YEAR)
+string = string.replace('FFFF',YEAR)
 
+# print(string)
 datas_bruto = string.split('\n')
 
-meses = ('Janeiro','Fevereiro', 'Março', 'Abril', 'Maio', 'Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro')
+meses = ('Janeiro','Fevereiro', 'Março', 'Abril', 'Maio', 'Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro','bob')
 calendario = {}
 mes = 0;
 ultimo_dia = -1
-calendario[meses[mes]] = []
+calendario[mes] = []
 
 for data in datas_bruto:
     if(data[0] != ' '):
         dia = data[0]+data[1]
-        calendario[meses[mes]].append((dia,data))
         if(int(dia) < ultimo_dia):
             mes = mes + 1
-            calendario[meses[mes]] = []
+            calendario[mes] = []
+        calendario[mes].append((dia,data))
+        ultimo_dia = int(dia)
 
 print(calendario)
